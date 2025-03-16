758143f54b598f9274e1e9d0e60c26cb3e795ed9
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-03-16 15:12:39 +0530

    done

--- sensai/app/page.jsx
+++ sensai/app/page.jsx
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      Subscribe to KajalCodes
-      <br />
-      <Button>Hello</Button>
+     <div className="grid-background"></div>
     </div>
   );
 }
