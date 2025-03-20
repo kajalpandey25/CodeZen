451268fc2534cdf37285b935da3b94bfe4122825
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2025-03-20 14:54:33 +0530

    done

--- sensai/components/header.jsx
+++ sensai/components/header.jsx
@@ -14,11 +14,12 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async() => {
+  await checkUser();
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -51,7 +52,6 @@ const Header = () => {
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuSeparator />
               <DropdownMenuItem>
                 <Link href={"/resume"} className="flex items-center gap-2">
                   <FileText className="h-4 w-4" />
